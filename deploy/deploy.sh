707e66c7f66304a35aca9149844b4746c1533a08
wilmardo <info@wilmardenouden.nl>
2019-03-26 09:49:32 +0100

    fix(deploy): Correct manifest-tool command

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,4 +6,6 @@ docker push ${DOCKER_NAMESPACE}/${CONTAINER_NAME}:${ARCH}
 echo "Install manifest-tool"
 wget https://github.com/estesp/manifest-tool/releases/download/v1.0.0-rc2/manifest-tool-linux-amd64 -O manifest-tool
 chmod +x manifest-tool
-./manifest-tool:latest manifest-tool push from-spec manifest.yml
+
+echo "Deploying manifest"
+./manifest-tool push from-spec ../manifest.yml
